a2d1a5a587884bcbf97a13efdc40ea5eca4a437e
Ajayverma1502 <av805488@gmail.com>
2025-05-19 11:55:58 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+df=pd.read_csv('redwine.csv')
+print(df)
+
+reg=linear_model.LinearRegression()
+reg.fit(df[['fixed acidity','alcohol','free sulfur dioxide']],df.quality)
+print(reg.predict([[8.9,9.3,22]]))
+
